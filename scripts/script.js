55ef2a85ae7d3236e35af8bfeda7d2dc5a3340a5
s202489 <mail.raulortega@gmail.com>
2023-11-05 13:41:33 +0100

    fix: inline bugfix styles floating card

--- scripts/script.js
+++ scripts/script.js
@@ -26,18 +26,22 @@ document.addEventListener("DOMContentLoaded", function () {
             let type_color;
             if (columns[5] === "Beer") {
                 type_color = "#ffa600";
+                console.log("beer");
             }
 
             else if (columns[5] === "AF Beer") {
                 type_color = "#bc5090";
+                console.log("af beer");
             }
 
             else if (columns[5] === "Cider") {
                 type_color = "#ff6361";
+                console.log("cider");
             }
 
             else {
                 type_color = "#58508d";
+                console.log("soda");
             }
             
             const card = document.createElement("div");
@@ -47,7 +51,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 <div class="card-img-container">
                     <img src="figures/logo.png" alt="item-image">
                     <div class="top-left">${columns[3]} x</div>
-                    <div class="top-right" style="color:${type_color}; background:${type_color}">${columns[3]} x</div>
+                    <div class="top-right" style="color:#58508d; background:#58508d">${columns[3]} x</div>
                 </div>
                 <b>Quantity:</b> ${columns[3]} <br>
                 <b>Type:</b> ${columns[5]} <br>
